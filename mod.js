16fab5e65246487c6c7208135726675dba3327ae
Zack Smith <zsmith@leisurelink.com>
2016-05-11 08:06:29 -0600

    Updated packages to latest versions. Updated .eslintrc for eslint@2. Cleaned up source code a little. Removed unneeded pretest build. Cleaned up tests

--- mod.js
+++ mod.js
@@ -4,25 +4,25 @@ import { readdir, readdirSync } from 'fs';
 
 const readdirAsync = promisify(readdir);
 
-function addPropertyFrom(dir){
+function addPropertyFrom(dir) {
   return (acc, cur) => {
     let propName = cur.split('.').map(_.camelCase).join('.');
+    // const propName = _(cur).split('.').map(_.camelCase).join('.');
     acc[propName] = require(`${dir}/${cur}`);
     return acc;
   };
 }
 
 function requireFiles(dir, files) {
-  return _.chain(files)
+  return _(files)
     .without('index.js')
     .filter(file => _.endsWith(file, '.js') || _.endsWith(file, '.json')) // Only grab js and json files
     .map(file => _.initial(file.split('.')).join('.')) // Grab all but the extension of the file name
-    .reduce(addPropertyFrom(dir), {}) // Build object of modules keyed by filename
-    .value();
+    .reduce(addPropertyFrom(dir), {}); // Build object of modules keyed by filename
 }
 
 const load = dir => requireFiles(dir, readdirSync(dir));
 
-load.loadAsync = dir => readdirAsync(dir).then(files => requireFiles(dir, files));
+load.loadAsync = dir => readdirAsync(dir).then(requireFiles.bind(null, dir));
 
 export default load;
