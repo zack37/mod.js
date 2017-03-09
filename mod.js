a231665c130e09cff07bd5c9c6b9bc5eda1a3d9b
Zack Smith <zack.t.smith37@gmail.com>
2017-03-09 14:41:25 -0700

    Time to update (#3)
    * Updated dependencies. Removed babel-preset-2015 in favor of babel-preset-env. Replaced lodash with Ramda and lodash.camelCase. Minor cleanups overall. Removed support for node < 4
    
    * Fixed issues with build. Renamed test files directory

--- mod.js
+++ mod.js
@@ -1,28 +1,32 @@
-import { promisify } from 'bluebird';
-import _ from 'lodash';
+import camelCase from 'lodash.camelcase';
+import R from 'ramda';
 import { readdir, readdirSync } from 'fs';
 
-const readdirAsync = promisify(readdir);
+R.endsWith = suffix => list => {
+  return R.equals(R.takeLast(suffix.length, list), suffix);
+};
 
-function addPropertyFrom(dir){
+function addPropertyFrom(dir) {
   return (acc, cur) => {
-    let propName = cur.split('.').map(_.camelCase).join('.');
-    acc[propName] = require(`${dir}/${cur}`);
+    const propName = R.map(camelCase, cur).join('.');
+    acc[propName] = require(`${dir}/${cur.join('.')}`);
     return acc;
   };
 }
 
-function requireFiles(dir, files) {
-  return _.chain(files)
-    .without('index.js')
-    .filter(file => _.endsWith(file, '.js') || _.endsWith(file, '.json')) // Only grab js and json files
-    .map(file => _.initial(file.split('.')).join('.')) // Grab all but the extension of the file name
-    .reduce(addPropertyFrom(dir), {}) // Build object of modules keyed by filename
-    .value();
-}
+const requireFiles = R.curryN(2, (dir, files) => {
+  return R.pipe(
+    R.without('index.js'), // Remove index.js
+    R.filter(R.either(R.endsWith('.js'), R.endsWith('.json'))), // Only grab js and json files
+    R.map(file => R.init(file.split('.'))), // Filename without extensions
+    R.reduce(addPropertyFrom(dir), {}) // Build object of modules keyed by filename with javascript naming conventions
+  )(files);
+});
 
 const load = dir => requireFiles(dir, readdirSync(dir));
 
-load.loadAsync = dir => readdirAsync(dir).then(files => requireFiles(dir, files));
+load.loadAsync = dir => new Promise((resolve, reject) => {
+  return readdir(dir, (err, files) => err ? reject(err) : resolve(files));
+}).then(requireFiles(dir));
 
 export default load;
--- test/mod.tests.js
+++ test/mod.tests.js
@@ -4,31 +4,21 @@ import load, { loadAsync } from '../mod';
 
 describe('mod.js', () => {
 
-  it('should be able to require load as a function (babel 6 sanity check)', () => {
-    const loadFromRequire = require('../');
-    expect(loadFromRequire).to.be.a('function');
-  });
-
-  it('should be able to require loadAsync as a function (babel 6 sanity check)', () => {
-    const loadAsyncFromRequire = require('../').loadAsync;
-    expect(loadAsyncFromRequire).to.be.a('function');
-  });
-
   describe('load', () => {
 
     let directory;
 
     before(() => {
-      directory = load(__dirname + '/loadTest');
+      directory = load(__dirname + '/testFiles');
     });
 
-    it('should load all normal files but index from loadTest/ into an object', () => {
+    it('should load all normal files but index from testFiles/ into an object', () => {
       expect(directory).to.have.property('one').that.is.eql({ name: 'one.js' });
       expect(directory).to.have.property('two').that.is.eql({ name: 'two.js' });
       expect(directory).to.not.have.property('index');
     });
 
-    it('should load json files from loadTest/ into an object', () => {
+    it('should load json files from testFiles/ into an object', () => {
       expect(directory).to.have.property('test').that.is.eql({ name: 'test.json' });
     });
 
@@ -51,30 +41,30 @@ describe('mod.js', () => {
     let directory;
 
     before(() =>  {
-      return loadAsync(__dirname + '/loadTest')
+      return loadAsync(__dirname + '/testFiles')
         .then(files => directory = files);
     });
 
-    it('should load all normal named files but index from loadTest/ into an object asynchronously', () => {
+    it('should load all normal named files but index from testFiles/ into an object asynchronously', () => {
       expect(directory).to.have.property('one').that.is.eql({ name: 'one.js' });
       expect(directory).to.have.property('two').that.is.eql({ name: 'two.js' });
       expect(directory).to.not.have.property('index');
     });
 
-    it('should load json files from loadTest/ into an object asynchronously', () => {
-      return expect(directory).to.have.property('test').that.is.eql({ name: 'test.json' });
+    it('should load json files from testFiles/ into an object asynchronously', () => {
+      expect(directory).to.have.property('test').that.is.eql({ name: 'test.json' });
     });
 
     it('should load files with dots in the name excluding extension asynchronously', () => {
-      return expect(directory).to.have.property('one.two').that.is.eql({ name: 'one.two.js' });
+      expect(directory).to.have.property('one.two').that.is.eql({ name: 'one.two.js' });
     });
 
     it('should load files with snake_casing as camelCasing asynchronously', () => {
-      return expect(directory).to.have.property('snakeCase').that.is.eql({ name: 'snake_case.js' });
+      expect(directory).to.have.property('snakeCase').that.is.eql({ name: 'snake_case.js' });
     });
 
     it('should load files with snake_casing and dots as camelCasing with dots asynchronously', () => {
-      return expect(directory).to.have.property('snakeCase.other').that.is.eql({ name: 'snake_case.other.js' });
+      expect(directory).to.have.property('snakeCase.other').that.is.eql({ name: 'snake_case.other.js' });
     });
 
   });
